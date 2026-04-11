8db789d4381c882bbcd77fb87b9584e980451e71
JaTaQue <jaime.tarazonaq@gmail.com>
2026-04-11 20:47:32 +0200

    IS_score looks functional enough I did some tests feel free to verify if you want

--- Metrics/is_score.py
+++ Metrics/is_score.py
@@ -1,11 +1,25 @@
 from __future__ import annotations
 
 from typing import Any, Dict
+import torch
+from torchvision import models
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+def compute_inception_score(preds, num_splits=10):
+    if isinstance(preds, np.ndarray):
+        preds = preds  # already a single array, use as-is
+    else:
+        preds = np.concatenate(preds, axis=0)  # list of arrays
+    
+    scores = []
+    N = preds.shape[0]
 
+    for k in range(num_splits):
+        part = preds[k * (N // num_splits): (k + 1) * (N // num_splits), :]
+        py = np.mean(part, axis=0)
+        kl_div = part * (np.log(part + 1e-10) - np.log(py + 1e-10))
+        kl_div = np.sum(kl_div, axis=1)
+        scores.append(np.exp(np.mean(kl_div)))
 
-def compute_inception_score(fake_samples: Any, **kwargs: Any):
-    """
-    TODO: implement Inception Score with split-based mean/std output
-    """
-    #  return keys: {"mean": float, "std": float}
-    raise NotImplementedError("TODO: implement compute_inception_score")
+    return np.mean(scores), np.std(scores)
