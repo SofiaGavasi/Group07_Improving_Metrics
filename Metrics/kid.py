d9ea86a044100f2835f60f505530db001adeaa5b
EIGursoy <ilyada1308@gmail.com>
2026-04-11 20:32:20 +0300

    kid updated

--- Metrics/kid.py
+++ Metrics/kid.py
@@ -2,14 +2,15 @@ from __future__ import annotations
 
 from typing import Any, Dict
 import numpy as np
-
+import torch
+from torchmetrics.image.kid import KernelInceptionDistance
 
 
 def polynomial_kernel(x: np.ndarray,y: np.ndarray):
     d = x.shape[1] # kernel on feature dimension
     return ((1/d)*(x @ y.T)+ 1 )**3
 
-def compute_kid(real_features: np.ndarray, fake_features: np.ndarray):
+def kid(real_features: np.ndarray, fake_features: np.ndarray):
     m= real_features.shape[0] # comparing based on samples
     n= fake_features.shape[0]
 
@@ -24,12 +25,26 @@ def compute_kid(real_features: np.ndarray, fake_features: np.ndarray):
 
     return float(kid)
 
-    ## TO DO: 
-    #   - add multible runs, so it calculates for many subsets
-    #   - add std 
-    #   - compare results with library 
+def compute_kid(real_features: np.ndarray, fake_features: np.ndarray):
+    KIDs= []
+    for _ in range(100):
+        random_real = np.random.choice(real_features.shape[0], 1000)
+        random_fake = np.random.choice(fake_features.shape[0], 1000)
+        sampled_kid = kid(real_features[random_real], fake_features[random_fake])
+        KIDs.append(sampled_kid)
 
+    return np.mean(KIDs) , np.std(KIDs)
 
+def kid_clean(real_images: np.ndarray, fake_images: np.ndarray):
 
-    #  return keys: {"mean": float, "std": float}
-    #raise NotImplementedError("TODO: implement compute_kid")
+    kid = KernelInceptionDistance(subset_size=100)
+    kid.update(real_images, real=True)
+    kid.update(fake_images, real=False)
+    kid_mean, kid_std = kid.compute()
+    return kid_mean, kid_std
+
+    ## Warning: 
+    #   - kid_clean uses images and not features 
+    # from documentation : RGB images of shape (3 x H x W) with dtype uint8. 
+    # All images will be resized to 299 x 299 which is the size of the original training data.
+
